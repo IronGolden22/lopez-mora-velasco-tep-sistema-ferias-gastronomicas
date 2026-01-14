70b2dbf633a1cbcdae9e2863015c3d365f54e23f
Mora-J <yeshua3000@gmail.com>
2026-01-13 22:10:41 -0400

    implementacion de cliente RPC para validar productos

--- apps/orders-service/src/orders/orders.module.ts
+++ apps/orders-service/src/orders/orders.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices'; // 👈 Importante
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
-import { Order } from './entities/order.entity'; 
+import { Order } from './entities/order.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order]), 
+    TypeOrmModule.forFeature([Order]),
+    ClientsModule.registerAsync([
+      {
+        name: 'PRODUCTS_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: 'localhost', 
+            port: configService.get<number>('PRODUCTS_RPC_PORT'), // Puerto 3006
+          },
+        }),
+      },
+    ]),
   ],
   controllers: [OrdersController],
   providers: [OrdersService],
--- apps/orders-service/src/orders/orders.service.ts
+++ apps/orders-service/src/orders/orders.service.ts
@@ -1,22 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
+import { ClientProxy } from '@nestjs/microservices'; // 👈 Importar ClientProxy
+import { firstValueFrom } from 'rxjs'; // 👈 Importar para convertir Observable a Promesa
 
 @Injectable()
-export class OrdersService { 
+export class OrdersService {
+  private readonly logger = new Logger(OrdersService.name);
+
   constructor(
     @InjectRepository(Order)
     private readonly orderRepository: Repository<Order>,
+    @Inject('PRODUCTS_SERVICE') private readonly productsClient: ClientProxy,
   ) {}
 
   async create(createOrderDto: CreateOrderDto) {
+    // 1. (Simulación) 
+    const productId = 1; 
+
+    this.logger.log(`Intentando conectar con Productos para validar ID: ${productId}...`);
+
+    try {
+      const productData = await firstValueFrom(
+        this.productsClient.send({ cmd: 'validate_product' }, { id: productId })
+      );
+
+      this.logger.log('Producto validado por RPC:', productData);
+      
+      // Aqui va la logica de: Si no hay stock, lanzar error.
+
+    } catch (error) {
+      this.logger.error('Error contactando a Productos:', error);
+      // Por ahora no bloqueamos el pedido si falla, solo logueamos el error
+    }
+
+    
     const newOrder = this.orderRepository.create(createOrderDto);
     return await this.orderRepository.save(newOrder);
   }
-
 
   async findAll() {
     return await this.orderRepository.find();
@@ -35,4 +59,4 @@ export class OrdersService {
     await this.orderRepository.delete(id);
     return { deleted: true, id };
   }
-} 
+}
