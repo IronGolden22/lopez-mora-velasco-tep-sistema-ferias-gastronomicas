76b97bd493452c0dade0bc431179d35b2307e724
IronGolden22 <gjlopez.24@est.ucab.edu.ve>
2026-01-13 21:46:57 -0400

    Se realizaron cambios para que la comunicacion del API con el microservicio fueran funcionales

--- apps/orders-service/src/orders/orders.controller.ts
+++ apps/orders-service/src/orders/orders.controller.ts
@@ -2,10 +2,35 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+// 1. IMPORTANTE: Agregar estos imports
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  // ======================================================
+  //  PARTE 1: MÉTODOS PARA EL API GATEWAY (TCP / Microservicio)
+  // ======================================================
+  
+  @MessagePattern({ cmd: 'create_order' })
+  createMicroservice(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern({ cmd: 'get_orders' })
+  findAllMicroservice() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'find_order' })
+  findOneMicroservice(@Payload() id: string) {
+    return this.ordersService.findOne(id);
+  }
+
+  // ======================================================
+  //  PARTE 2: MÉTODOS HTTP (Postman Directo al puerto 3006)
+  // ======================================================
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
